72ea039653093b49919fa065d03641da30a38d7a
Nimnapathum <pathumeshan123@gmail.com>
2024-09-15 02:31:02 +0530

    Create Service card component and it's css

---
+++ app/views/components/serviceCard.php
@@ -0,0 +1,44 @@
+<div class="service-card">
+    <div class="service-card-header">
+        <h3>Service No</h3>
+        <p class="service-card-status red-text">Done</p>
+    </div>
+    <div class="service-card-content">
+        <div class="field-group">
+            <div class="field-group">
+                <label class="service-card-label">Service_ID:</label>
+                <span class="service-card-field">11320</span>
+            </div>
+            <div class="field-group">
+                <label class="service-card-label">Price:</label>
+                <span class="service-card-field">2000 LKR</span>
+            </div>
+        </div>
+        <div class="field-group">
+            <div class="field-group">
+                <label class="service-card-label">Date:</label>
+                <span class="service-card-field">2024/07/26</span>
+            </div>
+            <div class="field-group">
+                <label class="service-card-label">Time:</label>
+                <span class="service-card-field">12.00 PM</span>
+            </div>
+        </div>
+        <div class="field-group">
+            <label class="service-card-label">Provider:</label>
+            <span class="service-card-field">Service Provider 1</span>
+        </div>
+        <div class="field-group">
+            <label class="service-card-label">Property Address:</label>
+            <span class="service-card-field">Address of the property</span>
+        </div>
+        <div class="field-group">
+            <label class="service-card-label">Description:</label>
+            <span class="service-card-field">This is for the description of the property</span>
+        </div>
+        <div class="field-group">
+            <label class="service-card-label">Contact Number:</label>
+            <span class="service-card-field">0703954031</span>
+        </div>
+    </div>
+</div>
