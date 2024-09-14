f8c2ad365aaec34f47b4d4c46839fa412b167414
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-09-14 10:59:18 +0530

    Update userUpdate.view.php

--- app/views/user_view/userUpdate.view.php
+++ app/views/user_view/userUpdate.view.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/userview.css">
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/styles.css"> 
-    <title>PrimeCare - Profile</title>
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/userUpdate.css">
+    <title>PrimeCare - Edit Profile</title>
 </head>
 
 <body>
