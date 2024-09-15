3b0e2cee12e427a3e9522f1a348034e4df224141
Nimnapathum <pathumeshan123@gmail.com>
2024-09-15 21:24:26 +0530

    set displayed file to property unit

--- app/views/userhome.view.php
+++ app/views/userhome.view.php
@@ -31,7 +31,7 @@
                 <button class="secondary-btn">Logout</button>
             </div>
             <div class="user_view-content_section">
-                <?php require_once 'serviceProvider/repairListing.view.php'; ?>
+                <?php require_once 'owner/propertyUnit.view.php'; ?>
             </div>
         </div>
     </div>
