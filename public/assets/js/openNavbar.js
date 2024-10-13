49c22e0682c84ccd358fd934adae6a1e3108d0bf
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-10-13 16:21:48 +0530

    fix: error in header

--- public/assets/js/openNavbar.js
+++ public/assets/js/openNavbar.js
@@ -3,5 +3,6 @@ const menuBtn = document.getElementById('menu-btn'); // Hamburger icon
 const navLinks = document.getElementById('nav-links'); // UL element
 
 menuBtn.addEventListener('click', () => {
+    console.log('clicked');
     navLinks.classList.toggle('open'); // Toggle the 'open' class to show/hide menu
 });
