81448feba8d1375e6620c8aeec3688d718dc0e06
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-10-24 15:41:59 +0530

    Create repairCard.php

---
+++ app/views/agent/repairCard.php
@@ -0,0 +1,33 @@
+<div class="repair-card">
+    <div class="repair-left-content">
+        <h3>Repair Request 01</h3>
+        <div class="detail-fields">
+            <div class="detail-fields-aligned">
+                <span class="details-labels-aligend"><strong>Property ID :</strong></span><span class="details-field-small">P1236</span>
+            </div>
+            <div class="detail-fields-aligned">
+                <span class="details-labels-aligend"><strong>Customer ID :</strong></span><span class="details-field-small">Ow001</span>
+            </div>
+        </div>
+        <div class="detail-fields">
+            <div class="detail-fields-aligned">
+                <span class="details-labels-aligend"><strong>Date:</strong></span><span class="details-field-small">2024/08/23</span>
+            </div>
+        </div>
+        <div class="detail-fields">
+            <div class="detail-fields-aligned">
+                <span class="details-labels-aligend"><strong>Property Address:</strong></span><span class="details-field-small">66/2 Marine Drive, Colombo</span>
+            </div>
+        </div>
+        <div class="detail-fields">
+            <div class="detail-fields-aligned">
+                <span class="details-labels-aligend"><strong>Description:</strong></span><span class="details-field-small">Door Lock Broken</span>
+            </div>
+        </div>
+    </div>
+    <div class="approval-right-content">
+        <button class="accept-btn">Accept</button>
+        <button class="decline-btn">Decline</button>
+        <img src="<?= ROOT ?>/assets/images/listing_alt.jpg" alt="property" class="repair-prop-img">
+    </div>
+</div>
