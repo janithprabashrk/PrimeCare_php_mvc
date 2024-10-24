a4c253fc0bb4e60a867cf7312087f4636f6242b4
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-10-24 12:11:33 +0530

    Update agent.sidebar.php
    Add Requested Tasks

--- app/views/agent/agent.sidebar.php
+++ app/views/agent/agent.sidebar.php
@@ -2,6 +2,7 @@
     <li><button class="btn">Dashboard</button></li>
     <li><button class="btn">Profile</button></li>
     <li><button class="btn">Pre Inspection</button></li>
+    <li><button class="btn">Requested Tasks</button></li>
     <li><button class="btn">Task Management</button></li>
     <li><button class="btn">Manage Booking</button></li>
 </ul>
